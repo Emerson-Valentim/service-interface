b8f9cb90f18274a7bb892e96ad357b4002a02a21
Emerson Valentim <emevalentim0@gmail.com>
2022-01-02 13:51:46 -0300

    feat: add two event handlers on front

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -4,6 +4,7 @@ import useSocket from "../../app/use-socket";
 
 export function Counter() {
   const [fish, setFish] = useState<string>("Unset");
+  const [jobTime, setJobTime] = useState<Date>();
 
   const socket = useSocket("socket-1");
 
@@ -12,10 +13,18 @@ export function Counter() {
       setFish(fish.specie);
     });
 
+    socket.on("client:fish:job:done", (epoch) => {
+      setJobTime(new Date(epoch.date));
+    });
+
     return () => {
       socket.disconnect();
     };
   });
 
-  return <div>{fish}</div>;
+  return (
+    <div>
+      Fish: {fish} - Created: {jobTime?.toISOString() || "Unset"}
+    </div>
+  );
 }
